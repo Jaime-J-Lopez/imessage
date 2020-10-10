7d999307c7f472b03c4f43c41f75c5d6668f55a9
Jaime-J-Lopez <geekwise.jaime.lopez@gmail.com>
2020-10-09 22:31:10 -0700

    chore(init): intial upload react create app

---
+++ src/Imessage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Imessage.css";
+function Imessage() {
+    return <div className="Imessage">
+        {/*{Sidebar}*/}
+        <Sidebar />
+        {/*{Chat}*/}
+        <Chat />
+
+    </div>;
+}
+
+export default Imessage;
